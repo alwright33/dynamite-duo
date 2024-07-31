805d2b0e497299d72255bc092efd7dabc2164d08
alwright33 <austinlucaswright@gmail.com>
2024-07-31 13:25:42 -0500

    Added HTML rendering module

--- scripts/movie-converter.js
+++ scripts/movie-converter.js
@@ -23,3 +23,18 @@ The movie description is wrapped in a <p> element with the class "movie__descrip
     //Finally, we return the movieHTML string containing the generated HTML markup for all the movies.
     return movieHTML
 }
+
+
+
+
+/*We created a JavaScript module that converts the movie data into HTML markup. By importing the movie data from the 'movies-data.js 
+file, we can access the [movies] array and iterate over each movie object. 
+
+Inside the 'generateMoviesHTML function, we use a 'for...of' loop to go through each movie object and generate the corresponding 
+HTML structure using template literals. We dynamically insert the movie poster, title, and description into the appropriate elements.
+
+The generated HTML markup for each movie is concatenated into the movieHTML string, which is then returned by the function. 
+This allows us to obtain the complete HTML markup for all the movies.
+
+By exporting the generateMoviesHTML function, we make it available for use in other modules, 
+such as the main module where we will render the movie cards to the DOM. */
---
+++ scripts/movie-render.js
@@ -0,0 +1,21 @@
+/*We define a function called renderMoviesToDOM that accepts a parameter movieHTML,
+ which represents the HTML markup of the movie cards.*/
+export const renderMoviesToDOM = (movieHTML) => {
+   
+    /*Inside the function, we use 'document.getElementById('movie-list')' to select the DOM element with the ID "movie-list".
+    This is the element where we want to insert the movie cards.*/
+    //We store the selected element in the movieList variable.
+    const movieList = document.getElementById('movie-list');
+    
+    /*We then check if the movieList element exists using an if statement:
+        If movieList exists (truthy), we set its innerHTML property to the movieHTML received as a parameter. 
+        This effectively inserts the movie cards HTML into the "movie-list" element.
+
+        If movieList is null or undefined (falsy), indicating that no element with the ID "movie-list" was found, we log an error message to the console using console.error().
+        This helps in identifying and debugging issues related to missing or incorrect DOM elements.*/
+    if (movieList) {
+        movieList.innerHTML = movieHTML;
+    } else {
+        console.error('Could not find element with id "movie-list"');
+    }
+ };
